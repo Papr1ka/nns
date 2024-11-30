3ef34e9c1efe5e94a2018c9e74dd38bc98542c17
Joe <kirillpavlov4214@gmail.com>
2024-11-30 14:37:02 +0300

    3 folder removed

--- 3/nn/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import Nums, NumsToNums, NumsNumsToNums
-from .layer import Layer
-from .nn import NN
-from .optimizers import *
-from .functions import *
--- 3/nn/base.py
+++
@@ -1,6 +0,0 @@
-from typing import Callable
-import numpy as np
-
-type Nums = np.ndarray[np.number]
-type NumsToNums = Callable[[Nums], Nums]
-type NumsNumsToNums = Callable[[Nums, Nums], Nums]
